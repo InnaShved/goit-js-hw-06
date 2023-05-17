c8bb83bd280918aea7776595dcbd59d19e291a71
innshv <60898106+InnaShved@users.noreply.github.com>
2023-05-17 15:08:23 +0300

    task 2 completed

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+function createItem(item) {
+  const newEL = document.createElement("li");
+  newEL.textContent = item;
+  newEL.classList.add('item');
+  return newEL;
+};
+
+const ingredientsListItems = ingredients.map(createItem);
+const ingredientsList = document.querySelector('#ingredients');
+
+
+ingredientsList.append(...ingredientsListItems);
